af37cdc07030ba8ed1b0de352a52b3b2760a263c
Jason McGill <mcgilljay@gmail.com>
2022-06-19 19:31:19 -0400

    need break

--- javascript.js
+++ javascript.js
@@ -1,4 +1,6 @@
-
+let sidebar = document.querySelector('#sidebar');
+let chooseDimensions = document.createElement('button')
+sidebar.appendChild(chooseDimensions)
 
 function genDivs(vert, horiz) {
     let container = document.querySelector('#container');
@@ -8,10 +10,10 @@ function genDivs(vert, horiz) {
         for (let j = 0; j < horiz; j++) {
             let cell = document.createElement('div');
             cell.className = 'gridsquare';
-            cell.innerText = i * vert + j;
+            cell.innerText = i * vert + j +1;
             row.appendChild(cell);
         }
     container.appendChild(row);
     }
 }
-genDivs(4,4);
+genDivs(8,8);
