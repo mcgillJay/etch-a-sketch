70b97263dad827f41fe6860f26b1ba1738d5a98f
Jason McGill <mcgilljay@gmail.com>
2022-06-19 18:50:06 -0400

    creating 16 div

---
+++ javascript.js
@@ -0,0 +1,17 @@
+
+
+function genDivs(vert, horiz) {
+    let container = document.querySelector('#container');
+    for (let i = 0; i < vert; i++) {
+        let row = document.createElement('div');
+        row.className = 'row';
+        for (let j = 0; j < horiz; j++) {
+            let cell = document.createElement('div');
+            cell.className = 'gridsquare';
+            cell.innerText = i * vert + j;
+            row.appendChild(cell);
+        }
+    container.appendChild(row);
+    }
+}
+genDivs(4,4);
