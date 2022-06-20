2b2c2624ea6e128306ac034622d710e83d97e635
Jason McGill <mcgilljay@gmail.com>
2022-06-20 19:05:59 -0400

    set dimensions working correctly

--- javascript.js
+++ javascript.js
@@ -1,9 +1,34 @@
-let sidebar = document.querySelector('#sidebar');
-let chooseDimensions = document.createElement('button')
-sidebar.appendChild(chooseDimensions)
+//let sidebar = document.querySelector('#sidebar');
+
+let chooseDimensions = document.querySelector('#chooseDimension');
+chooseDimensions.addEventListener("click", dimensions);
+
+document.querySelectorAll(gridsquare);
+gridsquare.addEventListener("mouseover", darkenSquare);
+
+
+let reset = document.createElement('button');
+reset.className = 'button';
+sidebar.appendChild(reset);
+
+
+function dimensions () {
+    let x = prompt ('choose x: ');
+    while (x>100 || x<0) {
+        x = prompt('choose x (must be between 0 and 100): ')
+    }
+    let y = prompt ('choose y: ');
+    while (x>100 || x<0) {
+        y = prompt('choose y (must be between 0 and 100): ')
+    }
+    genDivs(x,y);
+}
 
 function genDivs(vert, horiz) {
     let container = document.querySelector('#container');
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
     for (let i = 0; i < vert; i++) {
         let row = document.createElement('div');
         row.className = 'row';
@@ -16,4 +41,9 @@ function genDivs(vert, horiz) {
     container.appendChild(row);
     }
 }
-genDivs(8,8);
+
+function darkenSquare() {
+    document.gridsquare.backgroundColor = 888888
+}
+
+
